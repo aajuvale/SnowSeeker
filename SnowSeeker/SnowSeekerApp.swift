7d52c17debcea28c29e2d58a6a514fe9347c5407
aajuvale <ahmed.juvale@gmail.com>
2025-08-20 09:26:05 -0700

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Ahmed Juvale on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
